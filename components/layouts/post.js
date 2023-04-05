7e04276f0ed16436db559a3a7b929f9198726ca3
Rafael Turk <rafael@woovi.com>
2023-04-04 22:10:51 -0300

    feat(_skip): so we can merge

---
+++ components/layouts/post.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Page from "./main";
+import Head from "next/head";
+import Header from "../post/header";
+import { Suspense } from "react";
+
+const Post = ({ id, children, meta }) => {
+  const { title, date, description, og } = meta;
+  
+  return (
+    <Page>
+      <Head>
+        <meta property="og:title" content={title} />
+        <meta property="og:site_name" content="Rafael Turk's blog" />
+        <meta property="og:description" content={description} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@rafaturk" />
+        <meta property="og:image" content={og} />
+      </Head>
+          <main>
+            <article>
+              <Header id={id} title={title} date={date.toLocaleString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' })} />
+              <Suspense fallback={null}>{children}</Suspense>
+            </article>
+          </main>
+      <style jsx>{`
+        main {
+          padding: 15px;
+          font-size: 18px;
+        }
+
+        article {
+          scroll-margin-top: 50px;
+        }
+
+        @media (min-width: 500px) {
+          main {
+            max-width: 42rem;
+            margin: auto;
+          }
+        }
+      `}</style>
+    </Page>
+  );
+};
+
+export default Post;
